53e1d55a9346df685d303af031eeb568d8ae3398
Marek Michalica <mmichalica7@gmail.com>
2024-11-15 15:55:09 +0100

    HTTP protocol function

--- src/main.py
+++ src/main.py
@@ -17,18 +17,17 @@ def main():
     # Analyzovanie PCAP súboru a aplikovanie filtrov
     packets = analyze_packets(args.pcap_file, filters)
 
-    # Vizualizácia výsledkov ako koláčový graf
-    plot_protocols(packets["protocol_counts"], args.pcap_file)
-
     # Výpis paketov
     print("\nFiltered Packets:\n")
-    print("# | Timestamp            | Source IP       | Destination IP  | Protocol | Src Port | Dst Port | Packet Size")
-    print("-" * 95)
+    print("# | Timestamp            | Source IP       | Destination IP  | Protocol | Src Port | Dst Port | Packet Size | Data ")
+    print("-" * 100)
     for idx, packet_info in enumerate(packets["filtered_packets"], 1):
         print(f"{idx:<2} | {packet_info['timestamp']} | {packet_info['src_ip']:<15} | {packet_info['dst_ip']:<15} | "
               f"{packet_info['protocol']:<7} | {packet_info['src_port']:<8} | {packet_info['dst_port']:<8} | "
-              f"{packet_info['size']} bytes")
+              f"{packet_info['size']:<5} bytes | {packet_info['payload']} |")
 
+    #Vizualizácia výsledkov ako koláčový graf
+    plot_protocols(packets["protocol_counts"], args.pcap_file)
 
 if __name__ == "__main__":
     main()
