3ef3eec8a78fa7377319698aa698c70841908e16
Marek Michalica <mmichalica7@gmail.com>
2024-11-15 10:54:36 +0100

    Small modifications to the visualization

--- src/visualizations/protocol_distribution.py
+++ src/visualizations/protocol_distribution.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
 
-def plot_protocols(protocol_counts):
+def plot_protocols(protocol_counts, pcap_file):
     labels = protocol_counts.keys()
     sizes = protocol_counts.values()
 
-    plt.figure(figsize=(8, 8))
+    fileLocation = pcap_file[5:]
+
+    plt.figure(figsize=(9, 9))
     plt.pie(sizes, labels=labels, autopct="%1.1f%%", startangle=140)
-    plt.title("Protocol Usage in PCAP File")
+    plt.title("Protocol Usage in " + fileLocation)
     plt.show()
